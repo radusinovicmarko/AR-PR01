1b7a9bfc7ad41bb019a41e5d61fee1093ec76728
radusinovicmarko <radusinovicmarko12@gmail.com>
2021-12-21 21:27:56 +0100

    First phase

--- ISASimulator.java
+++ ISASimulator.java
@@ -3,6 +3,9 @@ package org.unibl.etf.ar;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,10 +21,10 @@ public class ISASimulator {
 	
 	//Set of keywords
 	public static final HashSet<String> keywords = new HashSet<>();
-	//Set of unary operators
-	public static final HashSet<String> unaryOperators = new HashSet<>();
-	//Set of binary operators
-	public static final HashSet<String> binaryOperators = new HashSet<>();
+	//Map of unary operators
+	public static final HashMap<String, Consumer<String>> unaryOperators = new HashMap<>();
+	//Map of binary operators
+	public static final HashMap<String, BiConsumer<String, String>> binaryOperators = new HashMap<>();
 	public static final String breakpoint = "BREAKPOINT";
 	
 	//Code to interpret
@@ -40,16 +43,16 @@ public class ISASimulator {
 	}
 	
 	public static void setKeywordsAndOperators() {
-		unaryOperators.add("NOT");
-		binaryOperators.add("ADD");
-		binaryOperators.add("SUB");
-		binaryOperators.add("AND");
-		binaryOperators.add("OR");
-		binaryOperators.add("MOV");
-		binaryOperators.add("SCAN");
-		binaryOperators.add("PRINT");
-		keywords.addAll(unaryOperators);
-		keywords.addAll(binaryOperators);
+		unaryOperators.put("NOT", ISASimulator::not);
+		binaryOperators.put("ADD", ISASimulator::add);
+		binaryOperators.put("SUB", ISASimulator::sub);
+		binaryOperators.put("AND", ISASimulator::and);
+		binaryOperators.put("OR", ISASimulator::or);
+		binaryOperators.put("MOV", ISASimulator::mov);
+		binaryOperators.put("SCAN", ISASimulator::scan);
+		binaryOperators.put("PRINT", ISASimulator::print);
+		keywords.addAll(unaryOperators.keySet());
+		keywords.addAll(binaryOperators.keySet());
 		keywords.add(breakpoint);
 	}
 	
@@ -87,19 +90,19 @@ public class ISASimulator {
 				return;
 			String keyword = s.split(" ")[0].toUpperCase();
 			String[] operands = s.substring(index + 1).replaceAll(" ", "").split(",");
-			if (unaryOperators.contains(keyword) && operands.length != 1)
+			if (unaryOperators.containsKey(keyword) && operands.length != 1)
 			{
 				isValid = false;
 				errorList.add(s);
 			}
-			else if (binaryOperators.contains(keyword) && operands.length != 2)
+			else if (binaryOperators.containsKey(keyword) && operands.length != 2)
 			{
 				isValid = false;
 				errorList.add(s);
 			}
 			Arrays.asList(operands).stream().forEach(o -> {
 				String oprnd = o.toUpperCase();
-				if (!oprnd.startsWith("[") && !registers.containsKey(oprnd)
+				if (!oprnd.startsWith("[") && !registers.containsKey(oprnd) && !isNumber(oprnd)
 						|| oprnd.startsWith("[") && !registers.containsKey(oprnd.substring(1, oprnd.length() - 1)))
 				{
 					isValid = false;
@@ -109,12 +112,114 @@ public class ISASimulator {
 		});
 	}
 	
+	public static void not(String arg) {
+		if (!arg.startsWith("[")) {
+			if (registers.containsKey(arg))
+				registers.put(arg, ~registers.get(arg));
+		}
+	}
+	
 	public static void add(String arg1, String arg2) {
+		if (!arg1.startsWith("["))
+		{
+			long result = 0;
+			if (!arg2.startsWith("[")) {
+				if (registers.containsKey(arg2))
+					result = registers.get(arg1) + registers.get(arg2);
+				else
+					result = registers.get(arg1) + Long.parseLong(arg2);
+			}
+			//else
+			registers.put(arg1, result);
+		}
+	}
+	
+	public static void sub(String arg1, String arg2) {
+		if (!arg1.startsWith("["))
+		{
+			long result = 0;
+			if (!arg2.startsWith("[")) {
+				if (registers.containsKey(arg2))
+					result = registers.get(arg1) - registers.get(arg2);
+				else
+					result = registers.get(arg1) - Long.parseLong(arg2);
+			}
+			//else
+			registers.put(arg1, result);
+		}
+	}
+	
+	public static void and(String arg1, String arg2) {
+		if (!arg1.startsWith("["))
+		{
+			long result = 0;
+			if (!arg2.startsWith("[")) {
+				if (registers.containsKey(arg2))
+					result = registers.get(arg1) & registers.get(arg2);
+				else
+					result = registers.get(arg1) & Long.parseLong(arg2);
+			}
+			//else
+			registers.put(arg1, result);
+		}
+	}
+	
+	public static void or(String arg1, String arg2) {
+		if (!arg1.startsWith("["))
+		{
+			long result = 0;
+			if (!arg2.startsWith("[")) {
+				if (registers.containsKey(arg2))
+					result = registers.get(arg1) | registers.get(arg2);
+				else
+					result = registers.get(arg1) | Long.parseLong(arg2);
+			}
+			//else
+			registers.put(arg1, result);
+		}
+	}
+	
+	public static void mov(String arg1, String arg2) {
+		if (!arg1.startsWith("["))
+		{
+			if (!arg2.startsWith("[")) {
+				if (registers.containsKey(arg2))
+					registers.put(arg1, registers.get(arg2));
+				else
+					registers.put(arg1, Long.parseLong(arg2));
+			}
+			//else
+		}
+	}
+	
+	//TODO
+	public static void scan(String arg1, String arg2) {
+		
+	}
+	
+	//TODO
+	public static void print(String arg1, String arg2) {
 		
 	}
 	
 	public static void interpretCode() {
-		
+		for (String line : code) {
+			line = line.trim();
+			String operator = line.split(" ")[0];
+			operator = operator.toUpperCase();
+			String args = line.substring(line.indexOf(' '));
+			args = args.replaceAll(" ", "");
+			args = args.toUpperCase();
+			if (unaryOperators.containsKey(operator)) {
+				unaryOperators.get(operator).accept(args);
+			}
+			else if (binaryOperators.containsKey(operator)) {
+				String arg1 = args.split(",")[0], arg2 = args.split(",")[1];
+				binaryOperators.get(operator).accept(arg1, arg2);
+			}
+			registers.keySet().stream().forEach(s -> System.out.println(s + " " + registers.get(s)));
+			System.out.println();
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -140,6 +245,15 @@ public class ISASimulator {
 			return;
 		}
 		interpretCode();
+	}
+	
+	private static boolean isNumber(String s) {
+		try {
+			Long.parseLong(s);
+			return true;
+		} catch(NumberFormatException e) {
+			return false;
+		}
 	}
 
 }
